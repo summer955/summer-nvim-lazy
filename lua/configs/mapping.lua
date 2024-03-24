bf9b0f451c42cc6aed6578f33f04c316dd676989
summer <1378617133@qq.com>
2024-03-24 20:12:29 +0800

    builtin.current_buffer_fuzzy_find

--- lua/configs/mapping.lua
+++ lua/configs/mapping.lua
@@ -19,6 +19,7 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fl", builtin.current_buffer_fuzzy_find, {})
 --trouble
 vim.keymap.set("n", "<leader>xx", function()
 	require("trouble").toggle()
