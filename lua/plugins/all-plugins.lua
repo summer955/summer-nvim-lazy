94e467f2f2bf56b00f94ccc664ef10efb248c944
summer <1378617133@qq.com>
2024-04-07 03:28:37 +0800

    com

--- lua/plugins/all-plugins.lua
+++ lua/plugins/all-plugins.lua
@@ -8,6 +8,13 @@ return {
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
+		-- opts = {
+		-- 	transparent = true,
+		-- 	styles = {
+		-- 		sidebars = "transparent",
+		-- 		floats = "transparent",
+		-- 	},
+		--},
 		config = function()
 			vim.cmd([[colorscheme tokyonight-moon]])
 		end,
--- lua/plugins/persisted.lua
+++ lua/plugins/persisted.lua
@@ -1,6 +1,8 @@
 -- Lua
 return {
 	"olimorris/persisted.nvim",
+	event = { "BufRead", "BufNewFile" },
+	cmd = { "Telescope persisted", "SessionLoad", "SessionLoadLast", "SessionLoadFromFile", "SessionToggle" },
 	config = true,
 	opts = {
 		autosave = true,
